76d4867f6d942830a984f371fe9a44a9610a29fa
Anthony Miceli <anthony080601@gmail.com>
2024-11-04 02:44:16 -0500

    maj et correction erreur home

---
+++ data.py
@@ -0,0 +1,43 @@
+database = [
+    # Agroalimentaire
+    {'nom': 'Saputo', 'ticker': 'SAP.TO', 'domaine': 'Agroalimentaire', 'suivi': False},
+    {'nom': 'Alimentation Couche-Tard', 'ticker': 'ATD.TO', 'domaine': 'Agroalimentaire', 'suivi': False},
+    {'nom': 'Loblaw', 'ticker': 'L.TO', 'domaine': 'Agroalimentaire', 'suivi': False},
+    {'nom': 'Maple Leaf Foods', 'ticker': 'MFI.TO', 'domaine': 'Agroalimentaire', 'suivi': False},
+    {'nom': 'Empire Company', 'ticker': 'EMP-A.TO', 'domaine': 'Agroalimentaire', 'suivi': False},
+    {'nom': 'Premium Brands', 'ticker': 'PBH.TO', 'domaine': 'Agroalimentaire', 'suivi': False},
+    {'nom': 'North West Company', 'ticker': 'NWC.TO', 'domaine': 'Agroalimentaire', 'suivi': False},
+    # Transport
+    {'nom': 'Canadian National Railway', 'ticker': 'CNR.TO', 'domaine': 'Transport', 'suivi': False},
+    {'nom': 'Canadian Pacific Kansas City', 'ticker': 'CP.TO', 'domaine': 'Transport', 'suivi': False},
+    {'nom': 'TFI International', 'ticker': 'TFII.TO', 'domaine': 'Transport', 'suivi': False},
+    {'nom': 'Westshore Terminals', 'ticker': 'WTE.TO', 'domaine': 'Transport', 'suivi': False},
+    {'nom': 'CAE Inc.', 'ticker': 'CAE.TO', 'domaine': 'Transport', 'suivi': False},
+    {'nom': 'Air Canada', 'ticker': 'AC.TO', 'domaine': 'Transport', 'suivi': False},
+    {'nom': 'Student Transportation', 'ticker': 'STB.TO', 'domaine': 'Transport', 'suivi': False},
+    # Énergie
+    {'nom': 'Suncor Energy', 'ticker': 'SU.TO', 'domaine': 'Énergie', 'suivi': False},
+    {'nom': 'Enbridge', 'ticker': 'ENB.TO', 'domaine': 'Énergie', 'suivi': False},
+    {'nom': 'TC Energy', 'ticker': 'TRP.TO', 'domaine': 'Énergie', 'suivi': False},
+    {'nom': 'Cenovus Energy', 'ticker': 'CVE.TO', 'domaine': 'Énergie', 'suivi': False},
+    {'nom': 'Canadian Natural Resources', 'ticker': 'CNQ.TO', 'domaine': 'Énergie', 'suivi': False},
+    {'nom': 'Pembina Pipeline', 'ticker': 'PPL.TO', 'domaine': 'Énergie', 'suivi': False},
+    {'nom': 'Gibson Energy', 'ticker': 'GEI.TO', 'domaine': 'Énergie', 'suivi': False},
+    {'nom': 'AltaGas', 'ticker': 'ALA.TO', 'domaine': 'Énergie', 'suivi': False},
+    # Services publics
+    {'nom': 'Fortis', 'ticker': 'FTS.TO', 'domaine': 'Services publics', 'suivi': False},
+    {'nom': 'Hydro One', 'ticker': 'H.TO', 'domaine': 'Services publics', 'suivi': False},
+    # Télécommunications
+    {'nom': 'Cogeco', 'ticker': 'CGO.TO', 'domaine': 'Télécommunications', 'suivi': False},
+    {'nom': 'Quebecor', 'ticker': 'QBR-B.TO', 'domaine': 'Télécommunications', 'suivi': False},
+    {'nom': 'Rogers', 'ticker': 'RCI-B.TO', 'domaine': 'Télécommunications', 'suivi': False},
+    {'nom': 'Telus', 'ticker': 'T.TO', 'domaine': 'Télécommunications', 'suivi': False},
+    # Technologie
+    {'nom': 'Shopify', 'ticker': 'SHOP.TO', 'domaine': 'Technologie', 'suivi': False},
+    {'nom': 'Constellation Software', 'ticker': 'CSU.TO', 'domaine': 'Technologie', 'suivi': False},
+    {'nom': 'BlackBerry', 'ticker': 'BB.TO', 'domaine': 'Technologie', 'suivi': False},
+    {'nom': 'Lightspeed', 'ticker': 'LSPD.TO', 'domaine': 'Technologie', 'suivi': False},
+    {'nom': 'Dye & Durham', 'ticker': 'DND.TO', 'domaine': 'Technologie', 'suivi': False},
+    {'nom': 'Kinaxis', 'ticker': 'KXS.TO', 'domaine': 'Technologie', 'suivi': False},
+    {'nom': 'Enghouse Systems', 'ticker': 'ENGH.TO', 'domaine': 'Technologie', 'suivi': False}
+    ]
--- home.py
+++ home.py
@@ -95,31 +95,31 @@ def render_home(database):
                 combined_df = pd.concat(valeurs, axis=1).mean(axis=1)
                 sector_averages[secteur] = combined_df
 
-                df_plot = pd.DataFrame(sector_averages)
-                df_plot.reset_index(inplace=True)
-                df_plot.rename(columns={"index": "Date"}, inplace=True)
+            df_plot = pd.DataFrame(sector_averages)
+            df_plot.reset_index(inplace=True)
+            df_plot.rename(columns={"index": "Date"}, inplace=True)
 
-                fig = make_subplots(specs=[[{"secondary_y": True}]])
-                for col in df_plot.columns[1:]:
-                    if col != "Technologie":
-                        fig.add_trace(
-                            go.Scatter(x=df_plot['Date'], y=df_plot[col], name=col),
-                            secondary_y=False
-                        )
-                fig.add_trace(
-                    go.Scatter(x=df_plot['Date'], y=df_plot['Technologie'], name='Technologie', line=dict(color='red')),
-                    secondary_y=True
-                )
+            fig = make_subplots(specs=[[{"secondary_y": True}]])
+            for col in df_plot.columns[1:]:
+                if col != "Technologie":
+                    fig.add_trace(
+                        go.Scatter(x=df_plot['Date'], y=df_plot[col], name=col),
+                        secondary_y=False
+                    )
+            fig.add_trace(
+                go.Scatter(x=df_plot['Date'], y=df_plot['Technologie'], name='Technologie', line=dict(color='red')),
+                secondary_y=True
+            )
 
-                fig.update_layout(
-                    title="Évolution moyenne des valeurs des actions par secteur au Canada",
-                    xaxis_title="Date",
-                    legend_title_text='Secteur'
-                )
-                fig.update_yaxes(title_text="Prix moyen ($CAD)", secondary_y=False)
-                fig.update_yaxes(title_text="Prix moyen ($CAD) - Technologie", secondary_y=True)
-                st.plotly_chart(fig)
-                # Function to calculate percentage change for each sector
+            fig.update_layout(
+                title="Évolution moyenne des valeurs des actions par secteur au Canada",
+                xaxis_title="Date",
+                legend_title_text='Secteur'
+            )
+            fig.update_yaxes(title_text="Prix moyen ($CAD)", secondary_y=False)
+            fig.update_yaxes(title_text="Prix moyen ($CAD) - Technologie", secondary_y=True)
+            st.plotly_chart(fig)
+            # Function to calculate percentage change for each sector
 
     def calculate_metrics(df):
         metrics = {}
