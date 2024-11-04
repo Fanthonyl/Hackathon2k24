9a372a95cdc21027764884cb0a01c460dcddf1ec
TheoGachet <theo.gachet.pro@outlook.fr>
2024-11-04 10:07:53 -0500

    home page

--- home.py
+++ home.py
@@ -57,9 +57,10 @@ st.markdown(
     }
     </style>
     <div class="center-text">
-        <p>Bienvenue ! Mon nom est Alex<span class="highlight">IA</span> (<span class="highlight">A</span>dvanced <span class="highlight">L</span>earning for <span class="highlight">E</span>conomic e<span class="highlight">X</span>ploration and <span class="highlight">I</span>nteractive <span class="highlight">A</span>nalysis) !</p>
-        <p>Je suis votre guide pour explorer les marchés financiers canadiens 👋 </p>
-        <p>Pour commencer, explorons les tendances des actions par secteur économique. Laissez Alex<span class="highlight">IA</span> se faire la voix des marchés !</p>
+        <h2>Bienvenue 👋 Mon nom est Alex<span class="highlight">IA</span> !</h2>
+        <h5>(<span class="highlight">A</span>dvanced <span class="highlight">L</span>earning for <span class="highlight">E</span>conomic e<span class="highlight">X</span>ploration and <span class="highlight">I</span>nteractive <span class="highlight">A</span>nalysis)</h5>
+        <p>Je suis votre guide pour explorer les marchés financiers canadiens. Pour commencer, explorons les tendances des actions par secteur économique.</p>
+        <p><span class="highlight">Laissez AlexIA se faire la voix des marchés !</span></p>
         <br>
     </div>
     """,
@@ -76,51 +77,6 @@ with col2:
         index=3
     )
 
-def get_data():
-    data = {}
-    for action in database:
-        try:
-            df = yf.download(action['ticker'], period=time_span)["Adj Close"]
-            data[action['domaine']] = data.get(action['domaine'], []) + [df]
-        except Exception as e:
-            pass
-    return data
-
-with col1:
-    # Show a spinner while loading the data and generating the plot
-    with st.spinner("AlexIA s'applique à dessiner..."):
-        data = get_data()
-        sector_averages = {}
-        for secteur, valeurs in data.items():
-            combined_df = pd.concat(valeurs, axis=1).mean(axis=1)
-            sector_averages[secteur] = combined_df
-
-        df_plot = pd.DataFrame(sector_averages)
-        df_plot.reset_index(inplace=True)
-        df_plot.rename(columns={"index": "Date"}, inplace=True)
-
-        fig = make_subplots(specs=[[{"secondary_y": True}]])
-        for col in df_plot.columns[1:]:
-            if col != "Technologie":
-                fig.add_trace(
-                    go.Scatter(x=df_plot['Date'], y=df_plot[col], name=col),
-                    secondary_y=False
-                )
-        fig.add_trace(
-            go.Scatter(x=df_plot['Date'], y=df_plot['Technologie'], name='Technologie', line=dict(color='red')),
-            secondary_y=True
-        )
-
-        fig.update_layout(
-            title="Évolution moyenne des valeurs des actions par secteur au Canada",
-            xaxis_title="Date",
-            legend_title_text='Secteur'
-        )
-        fig.update_yaxes(title_text="Prix moyen ($CAD)", secondary_y=False)
-        fig.update_yaxes(title_text="Prix moyen ($CAD) - Technologie", secondary_y=True)
-        st.plotly_chart(fig)
-        # Function to calculate percentage change for each sector
-
 def calculate_metrics(df):
     metrics = {}
     for col in df.columns[1:]:  # Skip the 'Date' column
@@ -133,19 +89,75 @@ def calculate_metrics(df):
         metrics[col] = change
     return metrics
 
-# Calculate metrics for each sector
-metrics = calculate_metrics(df_plot)
+def get_data():
+    data = {}
+    for action in database:
+        try:
+            df = yf.download(action['ticker'], period=time_span)["Adj Close"]
+            data[action['domaine']] = data.get(action['domaine'], []) + [df]
+        except Exception as e:
+            pass
+    return data
 
-# Display the metrics as columns
-st.subheader("Variation par secteur (en CAD)")
-metric_columns = st.columns(len(metrics))
+with col1:
+    # Show a spinner while loading the data and generating the plot
+        with st.spinner("AlexIA analyse en temps réel les marchés financiers..."):
+            data = get_data()
+            sector_averages = {}
+            for secteur, valeurs in data.items():
+                combined_df = pd.concat(valeurs, axis=1).mean(axis=1)
+                sector_averages[secteur] = combined_df
 
-for i, (sector, change) in enumerate(metrics.items()):
-    metric_columns[i].metric(
-        label=sector,
-        value=f"{df_plot[sector].iloc[-1]:.2f} $",
-        delta=f"{change:.2f}%"
-    )
+            df_plot = pd.DataFrame(sector_averages)
+            df_plot.reset_index(inplace=True)
+            df_plot.rename(columns={"index": "Date"}, inplace=True)
+
+            colors = [f'rgba(128, 0, {255 - i * 20}, 0.8)' for i in range(len(df_plot.columns) - 1)]
+
+            fig = make_subplots(specs=[[{"secondary_y": True}]])
+            indice = 0
+            for col in df_plot.columns[1:]:
+                
+                if col != "Technologie":
+                    fig.add_trace(
+                        go.Scatter(x=df_plot['Date'], y=df_plot[col], name=col, line=dict(color=colors[indice])),
+                        secondary_y=False
+                    )
+                else:
+                    index_tech = indice
+                indice +=1
+                
+            fig.add_trace(
+                go.Scatter(x=df_plot['Date'], y=df_plot['Technologie'], name='Technologie', line=dict(color=colors[index_tech])),
+                secondary_y=True
+            )
+
+            fig.update_layout(
+                title="Évolution moyenne des valeurs des actions par secteur au Canada",
+                xaxis_title="Date",
+                legend_title_text='Secteur'
+            )
+            fig.update_yaxes(title_text="Prix moyen ($CAD)", secondary_y=False)
+            fig.update_yaxes(title_text="Prix moyen ($CAD) - Technologie", secondary_y=True)
+            
+        # Calculate metrics for each sector
+        metrics = calculate_metrics(df_plot)
+
+        # Display the metrics as columns
+        #st.subheader("Variation par secteur (en CAD)")
+        metric_columns = st.columns(len(metrics))
+
+        for i, (sector, change) in enumerate(metrics.items()):
+            metric_columns[i].metric(
+                label=sector,
+                value=f"{df_plot[sector].iloc[-1]:.2f} $",
+                delta=f"{change:.2f}%"
+            )
+
+        st.plotly_chart(fig)
+        # Function to calculate percentage change for each sector
+
+
 
 
 # Downsample data only for the LLM prompt
@@ -171,7 +183,7 @@ session_id = str(uuid.uuid1())
 st.subheader("Insights financiers")
 
 # Show a spinner while waiting for the insights
-with st.spinner("AlexIA réfléchit profondément..."):
+with st.spinner("AlexIA rédige une analyse des tendance obtenues ci-dessus..."):
     try:
         insights = bedrock_wrapper.invoke_agent(agent_id, agent_alias_id, session_id, prompt)
         st.write(insights)
